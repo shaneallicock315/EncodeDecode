0e17ea0bcfb112b29d3b4fd4890928aaad5aa830
zander37 <149011001+zander37@users.noreply.github.com>
2023-10-31 21:58:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,20 @@ def encode(pswd):
   print("Your password has been encoded and stored")
   return encode_pass
 
+def decode(pswd):  # Decode Function
+    decoded_pass = ""
+    for char in pswd:
+        if int(char) == 2:
+            decoded_pass += "9"
+        elif int(char) == 1:
+            decoded_pass += "8"
+        elif int(char) == 0:
+            decoded_pass += "7"
+        else:
+            decoded_pass += str(int(char) - 3)
+    return decoded_pass
+
+
 def menu():
   while True:
 # menu function
@@ -42,7 +56,9 @@ def menu():
       pswd = (input("Please enter a password to encode: "))
       encoded = encode(pswd)
     elif option == 2:
-      pass
+      # decode option
+      decoded = decode(encoded)
+        print(f"The encoded password is {encoded}, and the original password is {decoded}.\n")
     elif option == 3:
       break
 
